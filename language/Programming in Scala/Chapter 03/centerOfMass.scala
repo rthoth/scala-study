a932df26f9256cee92554505eb6d88dc90593541
rthoth <ronaldo.asilva@gmail.com>
2012-09-23 11:58:39 -0300

    Changes...

--- language/Programming in Scala/Chapter 03/centerOfMass.scala
+++ language/Programming in Scala/Chapter 03/centerOfMass.scala
@@ -2,35 +2,32 @@
 	Hey, Can you search center of mass?
 */
 
-object CenterOfMass extends App {
-	
-	def centerOfMass(dots:List[Tuple2[Int, Int]]):Tuple3[Double,Double, Int] = {
-		if (dots == null || dots.isEmpty)
-			(0,0,0)
-		else if (dots.size == 1)
-			(dots.head._1, dots.head._2, 1)
-		else {
-			val me = dots.head;
-			val other = centerOfMass(dots.tail)
-			val ix = (me._1 - other._1) / (other._3 + 1)
-			val iy = (me._2 - other._2) / (other._3 + 1)
-			(other._1 + ix, other._2 + iy, other._3 + 1)
-		}
+def centerOfMass(dots:List[Tuple2[Int, Int]]):Tuple3[Double,Double, Int] = {
+	if (dots == null || dots.isEmpty)
+		(0,0,0)
+	else if (dots.size == 1)
+		(dots.head._1, dots.head._2, 1)
+	else {
+		val me = dots.head;
+		val other = centerOfMass(dots.tail)
+		val ix = (me._1 - other._1) / (other._3 + 1)
+		val iy = (me._2 - other._2) / (other._3 + 1)
+		(other._1 + ix, other._2 + iy, other._3 + 1)
 	}
-
-
-	val square = List((0,0), (1,0), (1,1), (0, 1))
-
-	println (centerOfMass(square))
-	println (centerOfMass(square.reverse))
-
-	val square2 = List((1,0), (0,0), (1,1), (0,1))
-
-	println (centerOfMass(square2))
-	println (centerOfMass(square2.reverse))
-
-	val square3 = List((0,0), (1,0), (0,1), (1,1))
-
-	println (centerOfMass(square3))
-	println (centerOfMass(square3.reverse))
 }
+
+
+val square = List((0,0), (1,0), (1,1), (0, 1))
+
+println (centerOfMass(square))
+println (centerOfMass(square.reverse))
+
+val square2 = List((1,0), (0,0), (1,1), (0,1))
+
+println (centerOfMass(square2))
+println (centerOfMass(square2.reverse))
+
+val square3 = List((0,0), (1,0), (0,1), (1,1))
+
+println (centerOfMass(square3))
+println (centerOfMass(square3.reverse))
